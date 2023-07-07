b36404f135fd5606e374c76578d9acbad5be6b3d
Betül KARAMAN <btlkrmn220@gmail.com>
2023-07-07 12:20:13 +0300

    designed login page

--- TpaoProject1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TpaoProject1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -45,6 +45,17 @@ namespace TpaoProject1.Areas.Identity.Pages.Account
             _emailSender = emailSender;
         }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
