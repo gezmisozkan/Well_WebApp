704a1fd629dd770ed21e0ad1ea66c3f7c9223bd2
melisayuncu <melisayuncu.my@gmail.com>
2023-07-13 09:09:43 +0300

    fixed controller problem

---
+++ TpaoProject1/Controllers/ViewWelltopsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TpaoProject1.Data;
+
+namespace TpaoProject1.Controllers
+{
+    public class ViewWelltopsController : Controller
+    {
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+
+        public IActionResult AddWellTops()
+        {
+            return View();
+        }
+
+
+        
+      
+    }
+}
